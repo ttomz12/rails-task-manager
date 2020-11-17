4445ce9778138b21066b398eca0ad3c693537031
Taro Tomiya <taro.tomiya@gmail.com>
2020-11-17 11:58:21 -0500

    Another migration

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,45 @@
 class TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def list
     @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(restaurant_params)
+    @task.save
+
+    redirect_to list_path
+  end
+
+  def edit
+    #@task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(restaurant_params)
+
+    redirect_to list_path
+  end
+
+  def remove
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    #@task = @tasks.find { |task| task["id"] == params[:id].to_i }
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'list', to: 'tasks#list'
+  get 'list/:id', to: 'tasks#show', as: :task
+
+  get 'new', to: 'tasks#new', as: :new
+  post 'list', to: 'tasks#create'
+
+  get 'list/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'list/:id', to: 'tasks#update'
 end
